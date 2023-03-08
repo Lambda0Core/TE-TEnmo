337d499a1f6ae74328eedec5ab21b6b5a5c7ff09
Emily LeFebvre <Emily@Lefebvre.us>
2023-03-08 17:07:22 -0500

    yet another slight addendum, left over code

--- tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
@@ -6,7 +6,7 @@ public class Account {
 
     private int accountId;
     private int userId;
-    private BigDecimal balance = new BigDecimal("1000");
+    private Balance balance;
 
     public Account(int accountId, int userId, Balance balance) {
         this.accountId = accountId;
@@ -30,11 +30,11 @@ public class Account {
         this.userId = userId;
     }
 
-    public BigDecimal getBalance() {
+    public Balance getBalance() {
         return balance;
     }
 
-    public void setBalance(BigDecimal balance) {
+    public void setBalance(Balance balance) {
         this.balance = balance;
     }
 }
